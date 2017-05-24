d1d843301fce26dd9219d81cddef3bd49c72e08e
Guillermo <galons01@ucm.es>
2017-05-24 10:18:43 +0200

    Barra de novedades

---
+++ includes/novedadesBar.php
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-inverse">
+	<ul class="nav navbar-nav">
+		<li class="active"><a href="novedades.html">Todo</a></li>
+		<li><a href="novedades-musica.html">Solo musica</a></li>
+		<li><a href="novedades-ilustraciones.html">Solo ilustraciones</a></li>
+	</ul>
+</nav>
