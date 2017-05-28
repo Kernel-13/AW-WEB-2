618a3fa8d91909e42eda83e2a9313bc0ab758dba
galons01 <galons01@ucm.es>
2017-05-28 20:56:31 +0200

    carusel(falta terminarlo)

---
+++ includes/carusel.php
@@ -0,0 +1,44 @@
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+	<!-- Indicators -->
+	<ol class="carousel-indicators">
+		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		<li data-target="#myCarousel" data-slide-to="1"></li>
+		<li data-target="#myCarousel" data-slide-to="2"></li>
+		<li data-target="#myCarousel" data-slide-to="3"></li>
+	</ol>
+
+	<!-- Wrapper for slides -->
+	<div id="carusel" class="carousel-inner" role="listbox">
+		<div class="item active">
+			<img class="audio-img-carusel" src="img/novedades/musica/stormtrooper.jpg" alt="Chania">
+			<audio class="audio-player-carusel" controls="controls"> <source src="novedades/musica/Monster_Cyborg_-_space_troops.mp3" type="audio/mpeg"> </audio>
+			<p>Un stormtrooper triunfa con su cancion "los blasters hacen pium pium"</p>
+		</div>
+
+		<div class="item">
+			<img class="ilust-carusel" src="img/novedades/img/chewaca.jpg" alt="Chania">
+			<p>El garabato de John ¿broma o arte?</p>
+		</div>
+
+		<div class="item">
+			<img class="ilust-carusel" src="img/heca2.jpg" alt="Flower">
+			<p>La primera imagen de LastXanadu</p>
+		</div>
+
+		<div class="item">
+			<img class="audio-img-carusel" src="img/novedades/musica/sparta.jpg" alt="Flower">
+			<audio class="audio-player-carusel" controls="controls"> <source src="resources/newfuries.mp3" type="audio/mpeg"> </audio>
+			<p>"300 ips no entran en 8 bits" de Leonidas.</p>
+		</div>
+	</div>
+
+	<!-- Left and right controls -->
+	<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		<span class="sr-only">Previous</span>
+	</a>
+	<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+		<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		<span class="sr-only">Next</span>
+	</a>
+</div>
