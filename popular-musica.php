fab9fdf2835fcdd73593f6f52c967f0aa642cebb
anabelcaiza <anabel92ycc@gmail.com>
2017-05-29 21:50:19 +0200

    Update popular-musica.php

--- popular-musica.php
+++ popular-musica.php
@@ -1,4 +1,3 @@
-<?php
 	session_start();
 ?>
 <!DOCTYPE html>
@@ -7,8 +6,8 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">	
-	<link rel="stylesheet" type="text/css" href="css/popular.css">
 	<link rel="stylesheet" type="text/css" href="css/body-style.css">
+	<link rel="stylesheet" type="text/css" href="css/popular.css">
 	<link rel="icon" href="img/hecate.ico" type="image/x-icon" />
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -16,25 +15,25 @@
 </head>
 <body>
 	<?php require "includes/navbar.php"; 
-	require ("includes/db.php"); 
-	?>
+	require ("includes/db.php");
+    ?> 
 	
     <!--AQUI-->  
     <div class="container">
 		<div class="col-md-8 col-xs-12">
-			<div class="row">
-        
-				<div id="cabecera">
-		                <h1>Top 10 canciones más populares </h1>
-		                <hr>
-		        </div>
-	<?php
-		$consulta = "SELECT * FROM users, posts where users.user_id=posts.post_owner and posts.post_type='Song' order by posts.post_views DESC limit 10";
-		$resultado = $mysqli->query($consulta) or die ($mysqli->error."en la linea".(__LINE__-1));
-		$cont = 1;
+			<div class="row"> 
+
+				<div id= "cabecera">
+					<h1>Top 10 Ilustraciones más populares </h1>
+					<hr>
+				</div>
+		<?php
+			$consulta = "SELECT * FROM users, posts where users.user_id=posts.post_owner and posts.post_type='Picture' order by posts.post_views DESC limit 10";
+			$resultado = $mysqli->query($consulta) or die ($mysqli->error."en la linea".(__LINE__-1));
+			$cont = 1;
 		
-			while ($row = mysqli_fetch_row($resultado)){
-				echo "<div class='ficha'>
+				while ($row = mysqli_fetch_row($resultado)){
+					echo "<div class='ficha'>
 						<div class='col-md-6 col-xs-12'>
 							<div class='col-md-2'>
 								<p class='top'>$cont</p>
@@ -47,7 +46,7 @@
 						<div class='col-md-6 col-xs-12 box-text'>
 							<h4>$row[11]</h4>
 							<h5><a href='user.php?id=$row[0]'>$row[1]</a></h5>
-							<a class='btn btn-primary' href='song.php?id=$row[0]'>Más información<span class='glyphicon glyphicon-chevron-right'></span></a>
+							<a class='btn btn-primary' href='illust.php?id=$row[0]'>Más información<span class='glyphicon glyphicon-chevron-right'></span></a>
 								<h5>$row[16] Reproducciones</h5>
 						</div>
 					  </div>
@@ -60,21 +59,22 @@
 		?>
 			</div>
 		</div>
-		
+
 		<!--Ultimos comentarios-->
-		<div class="col-md-4 col-sm-12">
-			<div class="list-group">
-				<h3>Noticias</h3>
-				<h4 class="headercomentario">Guitarra</h4>
-				<p>Esta semana se pone en cabeza Guitarra como la canción mas escuchada aunque sus comentarios no han sido los mejores.</p>
-				<h4 class="headercomentario">Rana</h4>
-				<p>Esta canción le pisa los talones muy de cerca al top 1 de esta semana.</p>
-				<h4 class="headercomentario">Mujer</h4>
-				<p>Se situa en el tercer puesto pero ha recibido los mejores comentarios de esta semana.</p>
+			<div class="col-sm-4">
+				<div class="list-group">
+					<h3>Noticias</h3>
+					<h4 class="headercomentario">Playa</h4>
+					<p>¿Una ilustracion infantil? Sea como sea se lleva el primer puesto de los mas vistos de esta semana.</p>
+					<h4 class="headercomentario">Ardilla</h4>
+					<p>En la segunda posición pero ha recibido las mejores votaciones de la semana</p>
+					<h4 class="headercomentario">Buzon</h4>
+					<p>El arte en una ilustración, se ha llevado los mejores comentarios.</p>
+				</div>
 			</div>
 		</div>
-	</div>
-
 	
+	<?php require "includes/PiePagina.php"; ?>
+    
 </body>
 </html>
