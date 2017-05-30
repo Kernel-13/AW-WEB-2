82d320344ac4da0d0eb02fa28d01fe2db37d0986
Ederson <funescastilloederson@gmail.com>
2017-05-30 11:27:53 +0200

    Listas de Usuarios OK

--- nuestrosMusicos.php
+++ nuestrosMusicos.php
@@ -1,116 +1,80 @@
 <?php 
-	session_start();
+session_start();
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
 
-<meta charset = "utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/ranking.css">
-    <link rel="icon" href="img/hecate.ico" type="image/x-icon" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <title>LastXanadu</title>
+	<meta charset = "utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/ranking.css">
+	<link rel="icon" href="img/hecate.ico" type="image/x-icon" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<title>LastXanadu</title>
 
 </head>
 <body>
-    <?php require "includes/navbar.php"; 
-		require ("includes/db.php"); 
+	<?php require "includes/navbar.php"; 
+	require ("includes/db.php"); 
 	?>    
-    <!--Desde aquí empezamos-->  
-    <div class="container">   
-        <!--contenido -->
-        <div class="col-sm-12">
-       <!--Cabecera-->
-        <div id="cabecera" class="row"> 
-                <h3>Nuestros músicos</h3>
-        </div>
-        <hr>
-		  <?php 
-            $consulta = "SELECT * FROM users, posts where users.user_id=posts.post_owner and posts.post_type='Song' order by posts.post_favourites DESC limit 10";
-            $resultado = $mysqli->query($consulta) or die ($mysqli->error."en la linea".(__LINE__-1));
-            $cont = 1;
-            while ($row = mysqli_fetch_row($resultado)){                                          
-            	echo '<div class="row">
-				<div class="col-xs-8 col-sm-8 col-md-4 col-lg-4">			
-					<img class="img-responsive" src="'.$row[15].'" alt="">
+
+	<div class="container">   
+
+		<div id="cabecera" class="row"> 
+			<div class="col-sm-12">
+				<br>
+				<h3>Nuestros Músicos</h3><br>
+				<hr>
+			</div>
+			<br>
+
+			<?php 
+			$consulta = "SELECT * FROM users WHERE user_type='Composer' order by user_name ASC";
+			$resultado = $mysqli->query($consulta) or die ($mysqli->error."en la linea".(__LINE__-1));
+
+			while ($row = $resultado->fetch_assoc()){                                          
+				echo '
+				<div class="row just-user-list">
+					<div class="col-xs-8 col-sm-8 col-md-4 col-lg-4">           
+						<a href="user.php?id='.$row["user_id"].'"><img class="img-responsive img-rounded" src="'.$row["user_avatar"].'" alt=""></a>
+					</div>
+					<div class="col-xs-8 col-sm-8 col-md-4 col-lg-8">                           
+						<h5 class = "titulo" ><a href="user.php?id='.$row["user_id"].'">'.$row["user_name"].'</a></h5>
+						<p>'.$row["user_description"].'</p>
+
+					</div>
 				</div>
 				<br/>
-  				<div class="col-xs-8 col-sm-8 col-md-4 col-lg-8">                           
-                     <h5 class = "titulo" ><a href="user.php?user='.$row[0].'">'.$row[1].'</a></h5>
-                     <h4>'.$row[12].'</h4>
+				<hr class="lineapunteada">
+				';   
 
+			}
 
-                </div>
+			$mysqli->close();
+			?>
+		</div>
 
-			</div>
-            <br>';
-            	
-            	
-            	echo '<br>';
-            	
-            	echo '<hr class="lineapunteada">';            	
-                    	
-            	$cont++;
-            }
-            
-            $mysqli->close();
-            ?>
+		<br>
 
-        
-                  
- <br>
+		<footer>
+			<ul class="list-inline">
+				<li><a href="quienesSomos.html">¿Quienes somos?</a></li>
+				<li><a href="contacto.html">Contacta con nosotros</a></li>
+				<li><a href="dondeEncontrarnos.html">Donde encontrarnos</a></li>
+				<li><a href="autoresWeb.html">Autores web</a></li>
+				<li><a href="actualizaciones.html">Actualizaciones y novedades de la pagina</a></li>
+			</ul>
+		</footer>
+	</div>
 
-					<ul class="pagination pagination-sm" align="center">
-						<li><a href="nuestrosMusicos.html">A</a></li>
-						<li><a href="nuestrosMusicos.html">B</a></li>
-						<li><a href="nuestrosMusicos.html">C</a></li>
-						<li><a href="nuestrosMusicos.html">D</a></li>
-						<li><a href="nuestrosMusicos.html">E</a></li>
-						<li><a href="nuestrosMusicos.html">F</a></li>
-						<li><a href="nuestrosMusicos.html">G</a></li>
-						<li><a href="nuestrosMusicos.html">H</a></li>
-						<li><a href="nuestrosMusicos.html">I</a></li>
-						<li><a href="nuestrosMusicos.html">J</a></li>
-						<li><a href="nuestrosMusicos.html">K</a></li>
-						<li><a href="nuestrosMusicos.html">L</a></li>
-						<li><a href="nuestrosMusicos.html">M</a></li>
-						<li><a href="nuestrosMusicos.html">N</a></li>
-						<li><a href="nuestrosMusicos.html">O</a></li>
-						<li><a href="nuestrosMusicos.html">P</a></li>
-						<li><a href="nuestrosMusicos.html">Q</a></li>
-						<li><a href="nuestrosMusicos.html">R</a></li>
-						<li><a href="nuestrosMusicos.html">S</a></li>
-						<li><a href="nuestrosMusicos.html">T</a></li>
-						<li><a href="nuestrosMusicos.html">U</a></li>
-						<li><a href="nuestrosMusicos.html">V</a></li>
-						<li><a href="nuestrosMusicos.html">X</a></li>
-						<li><a href="nuestrosMusicos.html">Y</a></li>
-						<li><a href="nuestrosMusicos.html">Z</a></li>
-						<li><a href="nuestrosMusicos.html">0..9</a></li></ul>
+	<!-- jQuery -->
+	<script src="js/jquery.js"></script>
 
-        <!-- pié -->
-        <footer>
-		<ul class="list-inline">
-			<li><a href="quienesSomos.html">¿Quienes somos?</a></li>
-			<li><a href="contacto.html">Contacta con nosotros</a></li>
-			<li><a href="dondeEncontrarnos.html">Donde encontrarnos</a></li>
-			<li><a href="autoresWeb.html">Autores web</a></li>
-			<li><a href="actualizaciones.html">Actualizaciones y novedades de la pagina</a></li>
-		</ul>
-	</footer>
+	<!-- Bootstrap Core JavaScript -->
+	<script src="js/bootstrap.min.js"></script>
 
-    </div>
-    <!-- /.container -->
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-	
 </body>
 </html>
