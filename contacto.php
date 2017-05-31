6ddc73e97af8fdb5d168b811e9bbc0f6cc12fd65
galons01 <galons01@ucm.es>
2017-05-31 07:55:22 +0200

    Arreglo de css

--- contacto.php
+++ contacto.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="icon" href="img/hecate.ico" type="image/x-icon" />
-    <link rel="stylesheet" type="text/css" href="css/body-style.css">
     <link rel="stylesheet" type="text/css" href="css/session-style.css">
+    <link rel="stylesheet" type="text/css" href="css/novedades.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src='https://www.google.com/recaptcha/api.js'></script>
